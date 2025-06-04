b7e6f537503fbe3693c6c9d7867c5bfc32fb9ecc
Viesturs71 <datoruguru@gmail.com>
2025-06-04 10:43:14 +0300

    Register company models with default admin

--- company/admin.py
+++ company/admin.py
@@ -1,19 +1,37 @@
+"""Admin configuration for the company app."""
+
 from django.contrib import admin
-from .models import Company, Department, Location
-from django.utils import timezone
+
 from myproject.admin import custom_admin_site
 
+from .models import Company, Department, Location
 
 
-@admin.register(Company, site=custom_admin_site)
 class CompanyAdmin(admin.ModelAdmin):
+    """Administration options for the ``Company`` model."""
+
     list_display = ("name", "code")
 
-@admin.register(Department, site=custom_admin_site)
+
 class DepartmentAdmin(admin.ModelAdmin):
+    """Administration options for the ``Department`` model."""
+
     list_display = ("name", "company")
 
-@admin.register(Location, site=custom_admin_site)
+
 class LocationAdmin(admin.ModelAdmin):
+    """Administration options for the ``Location`` model."""
+
     list_display = ("name", "address")
+
+
+# Register models with both the default admin site and the custom admin site so
+# that navigation links work regardless of which admin interface is used.
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Location, LocationAdmin)
+
+custom_admin_site.register(Company, CompanyAdmin)
+custom_admin_site.register(Department, DepartmentAdmin)
+custom_admin_site.register(Location, LocationAdmin)
 
